53879b1f0523ff16e6e0f5b6a41cf1f99c336da7
wonjae <berlaemrtls@gmail.com>
2023-04-25 13:00:44 +0900

    first_commit

---
+++ code/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,55 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    // 외부에서 넣어주는 memberService로 공통된 인스턴스를 쓰게 만든다. new()로 생성하면, 다른 인스턴스일 수 있다.
+    // @Autowired
+    // MemberService 생성자와 MemberRepository와 연결한다. 의존성 주입
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     */
+
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    // result.orElseGet(); // 값이 있으면 실행하고, 없으면 다른 default 값을 실행해
+
+}
